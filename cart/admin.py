a5dcf3f0e1a085af2f8117f8c956817bc3b9c427
HaRsH8747 <harshmoradiya8747@gmail.com>
2024-07-09 14:19:47 -0400

    cart admin register changed

--- cart/admin.py
+++ cart/admin.py
@@ -2,14 +2,18 @@
 from django.contrib import admin
 from .models import Cart, CartItem
 
-@admin.register(Cart)
+
 class CartAdmin(admin.ModelAdmin):
     list_display = ('user',)
     search_fields = ('user__username',)
     list_filter = ('user',)
 
-@admin.register(CartItem)
+
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ('cart', 'product', 'quantity')
     search_fields = ('cart__user__username', 'product__name')
     list_filter = ('cart', 'product')
+
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)
